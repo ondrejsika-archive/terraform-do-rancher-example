553cd5b491c71f55006b41139a00380dffc26411
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-25 22:38:36 +0200

    tf - bootstrap racher

--- terraform.tf
+++ terraform.tf
@@ -47,3 +47,26 @@ resource "cloudflare_record" "droplet" {
   type   = "A"
   proxied = false
 }
+
+provider "rancher2" {
+  alias = "bootstrap"
+
+  api_url   = "https://${cloudflare_record.droplet.hostname}"
+  bootstrap = true
+}
+
+resource "rancher2_bootstrap" "admin" {
+  provider = rancher2.bootstrap
+
+  password = "asdfasdf"
+  telemetry = true
+}
+
+output "rancher_api_url" {
+  value = rancher2_bootstrap.admin.url
+}
+
+output "rancher_token_key" {
+  value = rancher2_bootstrap.admin.token
+  sensitive = true
+}
